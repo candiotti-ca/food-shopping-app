3d857b328fc80873d6c21d8637ec0cace1a58d65
Carla Candiotti <carla.candiotti@hotmail.fr>
2023-11-08 13:56:03 +0100

    create ehading component

---
+++ src/components/Heading.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Heading } from "../types/theme";
+
+	export let tag: Heading = "h1";
+</script>
+
+<svelte:element this={tag} class="{tag}">
+	<slot/>
+</svelte:element>
--- src/types/theme.ts
+++ src/types/theme.ts
@@ -19,3 +19,11 @@ export type Variant =
 "soft" |
 "ringed" |
 "glass";
+
+export type Heading = 
+"h1" |
+"h2" |
+"h3" |
+"h4" |
+"h5" |
+"h6" ;
