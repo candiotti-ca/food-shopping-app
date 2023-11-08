a01b98a6a70562ad5d2705bcbc7f1a8a6c370853
Carla Candiotti <carla.candiotti@hotmail.fr>
2023-11-08 16:28:06 +0100

    update theme

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import type { Color, Size, Variant } from "../types/theme";
+	import type { Color, Size, Variant } from '../types/theme';
 
-	export let color: Color = "primary";
-	export let size: Size = "md";
-	export let variant: Variant = "filled";
+	export let color: Color = 'primary';
+	export let size: Size = 'md';
+	export let variant: Variant = 'filled';
 </script>
 
 <button type="button" class="btn btn-{size} variant-{variant}-{color}">
