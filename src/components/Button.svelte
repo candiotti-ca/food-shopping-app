9bc2ddbfb7f198dbafab795f847b5779f51e2872
Carla Candiotti <carla.candiotti@hotmail.fr>
2023-11-08 13:37:28 +0100

    create button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Color, Size, Style } from "../types/theme";
+
+	export let color: Color = "primary";
+	export let size: Size = "md";
+	export let style: Style = "filled";
+</script>
+
+<button type="button" class="btn btn-{size} variant-{style}-{color}">
+	<slot />
+</button>
---
+++ src/types/theme.ts
@@ -0,0 +1,20 @@
+export type Color = 
+"surface" | 
+"primary" | 
+"secondary" | 
+"tertiary" | 
+"warning" | 
+"error" | 
+"success";
+
+export type Size = 
+"sm" | 
+"md" | 
+"lg" | 
+"xl";
+
+export type Style = 
+"filled" |
+"ghost" |
+"soft" |
+"ringed";
