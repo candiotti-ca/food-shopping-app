269f0f255f7ac11b9cd558637ab1f52264e5d234
Carla Candiotti <carla.candiotti@hotmail.fr>
2023-11-08 13:46:22 +0100

    create alert

---
+++ src/components/Alert.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+	import type { Color, Style } from "../types/theme";
+
+	export let color: Color = "primary";
+	export let style: Style = "filled";
+</script>
+
+<aside class="alert variant-{style}-{color}" transition:fade={{ duration: 200 }}>
+	<div class="alert-message">
+		<slot/>
+	</div>
+</aside>
