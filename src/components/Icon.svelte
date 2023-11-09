ba1f4602f6d5f4318029ab880c9759db6629d69a
Carla Candiotti <carla.candiotti@hotmail.fr>
2023-11-09 16:00:11 +0100

    add trash icon to delete article

---
+++ src/components/Icon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Color, Size } from '../types/theme';
+
+	export let color: Color = 'primary';
+	export let size: Size = 'md';
+</script>
+
+<span class="text-{color}-500 text-{size}">
+	<slot />
+</span>
