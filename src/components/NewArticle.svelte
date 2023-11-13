3b6b36b0bf552874091753b085eff5678d736739
Carla <Carla.candiotti@hotmail.fr>
2023-11-13 15:28:49 +0100

    perform create with form tag

--- src/components/NewArticle.svelte
+++ src/components/NewArticle.svelte
@@ -5,19 +5,27 @@
 	import { shopping_articles } from '../stores';
 	import Button from './Button.svelte';
 	import Icon from './Icon.svelte';
+	import { enhance } from '$app/forms';
 
 	const dispatch = createEventDispatcher();
 	let articleName = '';
 
-	function addArticle() {
+	function addArticle(): void {
 		shopping_articles.addArticle(articleName);
 		dispatch('added');
 	}
 </script>
 
-<form on:submit={addArticle} class="alert variant-ringed-success" transition:fade>
+<form
+	on:submit={addArticle}
+	method="POST"
+	action="?/create"
+	class="alert variant-ringed-success"
+	transition:fade
+	use:enhance
+>
 	<div class="alert-message">
-		<input bind:value={articleName} class="w-full bg-transparent focus:outline-none" />
+		<input bind:value={articleName} class="w-full bg-transparent focus:outline-none" name="name" />
 	</div>
 
 	<div class="alert-actions">
